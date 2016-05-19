05cc5b62f0db20f56bdea381878887afde2c760d
Clay Dowling <clay@lazarusid.com>
2016-05-18 22:30:32 -0400

    Moved Executor system calls to use actual system object, so that it can be properly mocked out and tested.

--- newanimal/Executor.cpp
+++ newanimal/Executor.cpp
@@ -23,18 +23,24 @@ int Executor::run(map<string, string>& parameters)
 {
 	int size = command.size() + 1;
 	char** cli = new char* [size];
+    string arg;
     for(int i=0; i < command.size(); ++i) {
-        cli[i] = strdup(command[i].c_str());
+        arg = command[i];
+        if (parameters.find(arg != parameters.end())) {
+            cli[i] = strdup(parameters[arg].c_str());
+        } else {
+            cli[i] = strdup(arg.c_str());
+        }
     }
 
-    pid_t pid = fork();
+    pid_t pid = sys->fork();
     if (pid == 0) {
         // We're in the child process
         sys->execvp(command[0].c_str(), cli);
     } else {
         // We're waiting for our child, and probably late for work
         int status;
-        waitpid(pid, &status, 0);
+        sys->waitpid(pid, &status, 0);
 
         for(int i=0; i < command.size(); ++i) {
             free(cli[i]);
