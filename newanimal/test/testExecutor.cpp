a08ec691f88a577b48b157509c453e3a51b2dfe3
Clay Dowling <clay@lazarusid.com>
2016-05-25 21:17:15 -0400

    Additional characterization tests around Executor

--- newanimal/test/testExecutor.cpp
+++ newanimal/test/testExecutor.cpp
@@ -45,6 +45,62 @@ TEST_F(testExecutor, run_withSingleArgCli_waitpidCalledWithForkedPid)
 
     exe->setCli(cli);
     exe->run(params);
-
 }
 
+TEST_F(testExecutor, run_withFixedArguments_execvpCalledWithArgumentList)
+{
+    char* const* passedCli;
+
+    cli.push_back("ls");
+    cli.push_back("-l");
+    EXPECT_CALL(sys, fork())
+                .WillOnce(Return(0));
+    EXPECT_CALL(sys, execvp(StrEq("ls"), _))
+            .WillOnce(SaveArg<1>(&passedCli)); // Array matcher goes here
+
+    exe->setCli(cli);
+    exe->run(params);
+
+    ASSERT_THAT(passedCli[0], StrEq("ls"));
+    ASSERT_THAT(passedCli[1], StrEq("-l"));
+}
+
+TEST_F(testExecutor, run_withArgumentMatchingParameter_execvpCalledWithSubstitutedParameter)
+{
+    char* const* passedCli;
+
+    cli.push_back("ls");
+    cli.push_back("@FOLDER");
+    params["@FOLDER"] = "/tmp";
+
+    EXPECT_CALL(sys, fork())
+            .WillOnce(Return(0));
+    EXPECT_CALL(sys, execvp(StrEq("ls"), _))
+            .WillOnce(SaveArg<1>(&passedCli)); // Array matcher goes here
+
+    exe->setCli(cli);
+    exe->run(params);
+
+    ASSERT_THAT(passedCli[0], StrEq("ls"));
+    ASSERT_THAT(passedCli[1], StrEq("/tmp"));
+}
+
+TEST_F(testExecutor, run_withArgumentNotMatchingParameter_execvpCalledWithoutSubstitutedParameter)
+{
+    char* const* passedCli;
+
+    cli.push_back("ls");
+    cli.push_back("@FOLDER");
+    params["NOTFOLDER"] = "/tmp";
+
+    EXPECT_CALL(sys, fork())
+            .WillOnce(Return(0));
+    EXPECT_CALL(sys, execvp(StrEq("ls"), _))
+            .WillOnce(SaveArg<1>(&passedCli)); // Array matcher goes here
+
+    exe->setCli(cli);
+    exe->run(params);
+
+    ASSERT_THAT(passedCli[0], StrEq("ls"));
+    ASSERT_THAT(passedCli[1], StrEq("@FOLDER"));
+}
